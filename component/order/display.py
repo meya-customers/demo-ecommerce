419dcde53c1e2374c8378ea1f0faa31cbe216308
Erik Kalviainen <erik@meya.ai>
2020-08-26 16:35:20 -0400

    Minor improvements

--- component/order/display.py
+++ component/order/display.py
@@ -1,10 +1,8 @@
 from dataclasses import dataclass
-from meya.button.spec import ButtonSpec
+from meya.button.spec import ButtonElementSpec
 from meya.component.element import Component
 from meya.element.field import element_field
 from meya.entry import Entry
-from meya.orb.composer_spec import ComposerEventSpec
-from meya.orb.composer_spec import ComposerVisibility
 from meya.tile.event.ask import TileAskEvent
 from meya.tile.spec import TileButtonStyle
 from meya.tile.spec import TileCell
@@ -23,7 +21,7 @@ class OrderDisplayComponent(Component):
         tiles = []
         triggers = []
         for order in self.orders:
-            button = ButtonSpec(text="Select", result=order.id)
+            button = ButtonElementSpec(text="Select", result=order.id)
             buttons = self.get_buttons_and_triggers([button])
             triggers += buttons.triggers
 
